4075bfb3f3d81f9571cc4b7d3602c808ae16abc9
Joao Pedro <dev.joaopedrovieira@gmail.com>
2022-01-14 12:23:37 +0000

    First Commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { Article } from './Article'
+export { Brand } from './Brand'
+export { Cta } from './Cta'
+export { Feature } from './Feature'
+export { Navbar } from './Navbar'
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { Blog } from './Blog';
+export { Features } from './Features';
+export { Footer } from './Footer';
+export { Header } from './Header';
+export { Possibility } from './Possibility';
+export { WhatGpt3 } from './WhatGpt3';
